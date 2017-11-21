1e1f24e15d7af936d5408eeaa714c3e19b0933a5
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-21 18:40:46 +0700

    xon

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => res.render('home'));
 
 app.get('/word', (req, res) => {
     //render table words - like remove, update
-    res.render('words');
+    res.render('words', { words });
 });
 
 // app.post('/dangnhap', parser, (req, res) => {
